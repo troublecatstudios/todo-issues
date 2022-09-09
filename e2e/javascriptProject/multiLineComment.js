db54a79cc91fe86e28d7f2e6b932620bfb1da840
Jared Barboza <jared.m.barboza@gmail.com>
2022-09-09 10:27:37 -0400

    test: build out e2e tests

---
+++ e2e/javascriptProject/multiLineComment.js
@@ -0,0 +1,4 @@
+/* TODO: this is an example comment title
+This is the first line of the body.
+This will be the second line of the body
+*/
---
+++ e2e/javascriptProject/various-comment-examples.js
@@ -0,0 +1,11 @@
+
+// TODO: This comment is not linked to a GitHub Issue
+// TODO [#1453]: This comment is linked to a GitHub Issue
+
+// FIXME: [#132]: Linked to a github issue
+// And this command has a comment body!
+
+// BUG: Testing a simple bug comment with code
+for(var i = 0; i < 100; i++) {
+  console.log('hi there!');
+}

afd51665a71dcf4f0ee536cadad70b24ea5d78d4
Jared Barboza <jared.m.barboza@gmail.com>
2022-09-09 10:26:03 -0400

    test: add more grammar test cases

--- src/__tests__/grammar.tests.ts
+++ src/__tests__/grammar.tests.ts
@@ -26,18 +26,46 @@ describe('getTokens', () => {
   });
 
   describe('when the given file matches a prism grammar', () => {
-    it('should return the tokens for the file, with start and end line numbers', async () => {
-      let file = fixture('./only-multi-comment.js');
+    const fixtures = [
+      {
+        fixture: fixture('./only-multi-comment.js'),
+        expect: [
+          { line: 1, endLine: 4, valid: /of the body/mig }
+        ]
+      },
+      {
+        fixture: fixture('./todo-single-comment.js'),
+        expect: [
+          { line: 2, endLine: 2, valid: /convert this to typescript/mig }
+        ]
+      },
+      {
+        fixture: fixture('./various-comment-examples.js'),
+        expect: [
+          { line: 2, endLine: 2, valid: /is not linked/mig },
+          { line: 3, endLine: 3, valid: /is linked/mig }
+        ]
+      },
+    ];
+    it.each(fixtures)('should return the tokens for the file, with start and end line numbers', async (fx) => {
+      let file = fx.fixture;
       let result = await getTokens(file);
 
       let fileLines = (await readFile(file)).toString().split('\n').length;
 
-      let first = result.at(0);
+      let comments = result.filter(r => r.token instanceof Prism.Token && (r.token as Prism.Token).type === 'comment');
       expect(result).toBeInstanceOf(Array);
-      expect(first?.line).toBe(1);
-      expect(first?.endLine).toBe(4);
-      expect(first?.token).toBeInstanceOf(Prism.Token);
-      expect(first?.token).toHaveProperty('type', 'comment');
+      expect(comments.length).toBe(fx.expect.length);
+      for(let i = 0; i < comments.length; i++) {
+        let e = fx.expect[i];
+        let c = comments[i];
+
+        expect(c?.line).toBe(e.line);
+        expect(c?.endLine).toBe(e.endLine);
+        expect(c?.token).toBeInstanceOf(Prism.Token);
+        expect(c?.token).toHaveProperty('type', 'comment');
+        expect((c?.token as Prism.Token).content).toMatch(e.valid);
+      }
     });
   });
 });

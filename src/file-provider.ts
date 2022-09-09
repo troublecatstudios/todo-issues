5d52edaad9892a7e3fcc00754671b9a528d8b89a
Jared Barboza <jared.m.barboza@gmail.com>
2022-09-09 10:26:17 -0400

    fix: ignore directories

--- src/file-provider.ts
+++ src/file-provider.ts
@@ -2,7 +2,7 @@ import { create } from "@actions/glob";
 
 
 export const getFiles = async function(workingDirectory: string, ...globs: string[]): Promise<string[]> {
-  let globber = await create(globs.join('\n'));
+  let globber = await create(globs.join('\n'), { matchDirectories: false });
   let lastCwd = process.cwd();
   process.chdir(workingDirectory);
   let files = await globber.glob();

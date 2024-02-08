b138cf59ae8a4ff9440e1733a5acf75668b9ae5c
Jared Barboza <jared.m.barboza@gmail.com>
2024-02-08 06:19:05 -0500

    copy linguist languages.yml to dist and src

--- .github/scripts/build-helpers/copy.js
+++ .github/scripts/build-helpers/copy.js
@@ -1,15 +1,19 @@
 const path = require('path');
 const { directories, cp } = require('./utils');
 
-const filesToCopy = {
-  './src/issues/.template.eta': './dist/.template.eta'
-};
+const linguistPath = path.resolve(directories.root, './node_modules/linguist/lib/linguist/languages.yml');
+
+const filesToCopy = [
+  [path.resolve(directories.src, './issues/.template.eta'), path.resolve(directories.dist, '.template.eta')],
+  [linguistPath, path.resolve(directories.dist, './languages.yml')],
+  [linguistPath, path.resolve(directories.src, './issues/languages.yml')]
+];
 
 (async function() {
-  for(const key of Object.keys(filesToCopy)) {
-    const value = filesToCopy[key];
+  for(const set of filesToCopy) {
+    const [key, value] = set;
     process.stdout.write(`[copy] copying ${key} to ${value}...`);
-    await cp(path.resolve(directories.root, key), path.resolve(directories.root, value));
+    await cp(key, value);
     process.stdout.write(`done!\n`);
   }
 })();
